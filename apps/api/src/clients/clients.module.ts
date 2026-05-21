4d263d91c437bfcdf1dfbe154df713839b175dc2
haesookimDev <ww232330@gmail.com>
2026-05-21 14:27:35 +0900

    feat(api): Register ClientsGateway as provider

--- apps/api/src/clients/clients.module.ts
+++ apps/api/src/clients/clients.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClientJwtService } from './client-jwt.service';
+import { ClientsGateway } from './clients.gateway';
 import { ClientsInternalController } from './clients.internal.controller';
 import { ClientsPublicController } from './clients.public.controller';
 import { ClientsService } from './clients.service';
 
 @Module({
   controllers: [ClientsInternalController, ClientsPublicController],
-  providers: [ClientJwtService, ClientsService],
+  providers: [ClientJwtService, ClientsService, ClientsGateway],
   exports: [ClientJwtService, ClientsService],
 })
 export class ClientsModule {}
